939a7106e7557afb7cd5826b9b7cbdc98142a59c
*kem <109383619+KevinDavid99@users.noreply.github.com>
2026-09-11 07:37:44 +0100

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        s_count = {}
+        t_count = {}
+
+        for letter in range(len(s)):
+            if s[letter] in s_count:
+                s_count[s[letter]] +=1
+            else:
+                s_count[s[letter]] = 1
+
+        for letter in range(len(t)):
+            if t[letter] in t_count:
+                t_count[t[letter]] +=1
+            else:
+                t_count[t[letter]] = 1
+        return s_count == t_count
